bd8253e6e4eae8a5aa4a935c28c9bf7a8f19f8b2
Fred Jean <fred@commmandshiftn.com>
2015-09-21 16:17:44 -0600

    Initial commit.

--- README.md
+++ README.md
@@ -1,22 +1,17 @@
 # API Challenge
 
-Design and create an API that could be used to pull
-down a list of Pug images.
-
-We provide you with a starter project, you fill in the rest.
-
-# Dependencies
+## Dependencies
 
 This project uses Maven for builds.
 You need Java 8 installed.
 
-# Building
+## Building
 
 ```
 $ mvn package
 ```
 
-# Running
+## Running
 
 ```
 $ java -jar target/api_interview-0.1.0.jar
---
+++ src/main/java/com/disney/studios/Application.java
@@ -0,0 +1,16 @@
+package com.disney.studios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Bootstraps the Spring Boot com.disney.studios.Application
+ *
+ * Created by fredjean on 9/21/15.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
